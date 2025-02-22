ff4cd5e676141368247fe80db608775e46e166da
wagnerfloresdossantos <wagner.fs@aluno.ifsc.edu.br>
2025-02-22 10:43:00 -0300

    Atualização na pasta poo-2024.1

--- poo-2024.1/Revisoes/Aula20240812-Pilha/app/src/main/java/engtelecom/poo/PilhaCheiaException.java
+++
@@ -1,7 +0,0 @@
-package engtelecom.poo;
-
-public class PilhaCheiaException extends Exception {
-    public PilhaCheiaException(String message) {
-        super(message);
-    }
-}
--- poo-2024.1/Revisoes/Aula20240812-Pilha/app/src/main/java/engtelecom/poo/PilhaVaziaExeception.java
+++
@@ -1,7 +0,0 @@
-package engtelecom.poo;
-
-public class PilhaVaziaExeception extends Exception {
-    public PilhaVaziaExeception(String message) {
-        super(message);
-    }
-}
